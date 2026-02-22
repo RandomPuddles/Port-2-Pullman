7048b21129c619007cc9b5222396bc0e80bbfda9
Ian Allenfort <ianallenfort@yahoo.com>
2026-02-22 03:38:53 -0800

    Tried to fix UI2

--- EventTriggerAlarm/app/build.gradle.kts
+++ EventTriggerAlarm/app/build.gradle.kts
@@ -68,6 +68,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.core)
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.navigation.compose)
 
     // --- Room ---
     implementation(libs.room.runtime)
